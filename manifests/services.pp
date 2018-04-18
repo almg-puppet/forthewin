cdf0f4c51e481878f38fb153bba7d247e3b72875
Tiago M Delboni <tiago.delboni@almg.gov.br>
2018-04-18 12:33:40 -0300

    Refactoring of forthewin::services.

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,41 +1,31 @@
-# Manage Windows services 
+# Manage Windows services
 class forthewin::services (
-  Enum['true', 'false'] $started_services_enable = 'true',
-  Enum['true', 'false'] $stopped_services_enable = 'false',
-
-
+  Boolean $started_services_enable = true,
+  Boolean $stopped_services_enable = false,
   ) inherits forthewin::params {
+
+  info("[${trusted[certname]}] PARAMETERS & VARIABLES:")
+  info("[${trusted[certname]}] started_services_enable = ${started_services_enable}")
+  info("[${trusted[certname]}] stopped_services_enable = ${stopped_services_enable}")
 
   $started_services = hiera_array('forthewin::services::started_services')
   $stopped_services = hiera_array('forthewin::services::stopped_services')
 
-  
-  info('PARAMETERS:')
-  info("started_services_enable = ${started_services_enable}")
-  info("started_services = ${started_services}")
-  info("stopped_services_enable = ${stopped_services_enable}")
-  info("stopped_services = ${stopped_services}")
+  info("[${trusted[certname]}] started_services        = ${started_services}")
+  info("[${trusted[certname]}] stopped_services        = ${stopped_services}")
 
-
-  unless $forthewin::params::platform == 'wxp' {
-	if ! empty($started_services) {
-		$started_services.each |$service| { 
-			service { "$service":
-				ensure => "running",
-				enable => $started_services_enable
-			}
-		}
+	unless empty($started_services) {
+    service { $started_services:
+      ensure => running,
+      enable => $started_services_enable
+    }
 	}
 
-	if ! empty($stopped_services) {
-		$stopped_services.each |$service| { 
-			service { "$service":
-				ensure => "stopped",
-				enable => $stopped_services_enable
-			}
-		}
-	}
+	unless empty($stopped_services) {
+    service { $stopped_services:
+      ensure => stopped,
+      enable => $stopped_services_enable
+    }
+  }
 
-	
-}
 }
