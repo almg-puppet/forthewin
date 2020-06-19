df4c67ec74af5da84e57e1d9a77b451513559f34
Tiago M Delboni <tiago.delboni@almg.gov.br>
2020-06-19 18:35:07 -0300

    Improvement: installfor_productname parameter controls in which equipment model the software is installed. If installfor_productname is not provided, then it will be installed in *ANY* Dell model.

--- manifests/dellcc.pp
+++ manifests/dellcc.pp
@@ -2,6 +2,7 @@ class forthewin::dellcc (
   Array[String] $install_options = [],
   Optional[String] $installer_filename = undef,
   String $installer_path = "${forthewin::params::repo_basepath}\\dell",
+  Array[String] $installfor_productname = [],
   Boolean $verbose = $forthewin::params::verbose,
   Pattern[/\A[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+\Z/] $version
   ) inherits forthewin::params {
@@ -13,21 +14,34 @@ class forthewin::dellcc (
     $installer = "${installer_path}\\Command_Configure-${version}.msi"
   }
 
-  # Equipament's manufacturer
+  # Equipament's manufacturer and model
   $manufacturer = $facts[manufacturer]
+  $is_dell = $manufacturer == 'Dell Inc.'
+  $productname = $facts[productname]
+
+  # If installfor_productname is not provided, then install for *ANY* Dell model
+  if empty($installfor_productname) {
+    $condition = $is_dell
+  } else {
+    $condition = $is_dell and $productname in $installfor_productname
+  }
 
   if $verbose {
     info("[${trusted[certname]}] PARAMETERS:")
-    info("[${trusted[certname]}] install_options    = ${install_options}")
-    info("[${trusted[certname]}] installer_filename = ${installer_filename}")
-    info("[${trusted[certname]}] installer_path     = ${installer_path}")
-    info("[${trusted[certname]}] version            = ${version}")
+    info("[${trusted[certname]}] install_options        = ${install_options}")
+    info("[${trusted[certname]}] installer_filename     = ${installer_filename}")
+    info("[${trusted[certname]}] installer_path         = ${installer_path}")
+    info("[${trusted[certname]}] installfor_productname = ${installfor_productname}")
+    info("[${trusted[certname]}] version                = ${version}")
     info("[${trusted[certname]}] VARIABLES:")
-    info("[${trusted[certname]}] installer          = ${installer}")
-    info("[${trusted[certname]}] manufacturer       = ${manufacturer}")
+    info("[${trusted[certname]}] condition              = ${condition}")
+    info("[${trusted[certname]}] installer              = ${installer}")
+    info("[${trusted[certname]}] is_dell                = ${is_dell}")
+    info("[${trusted[certname]}] manufacturer           = ${manufacturer}")
+    info("[${trusted[certname]}] productname            = ${productname}")
   }
 
-  if $manufacturer == 'Dell Inc.' {
+  if $condition {
     package { 'Dell Command | Configure':
       ensure          => $version,
       source          => $installer,
