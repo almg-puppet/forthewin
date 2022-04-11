997e06b135bc06f3f010bbbea351e895fcac67b2
Tiago M Delboni <tiago.delboni@almg.gov.br>
2022-04-11 12:15:14 -0300

    +Verbosity control

--- manifests/services.pp
+++ manifests/services.pp
@@ -2,17 +2,20 @@
 class forthewin::services (
   Boolean $started_services_enable = true,
   Boolean $stopped_services_enable = false,
+  Boolean $verbose = $forthewin::params::verbose,
   ) inherits forthewin::params {
-
-  info("[${trusted[certname]}] PARAMETERS & VARIABLES:")
-  info("[${trusted[certname]}] started_services_enable = ${started_services_enable}")
-  info("[${trusted[certname]}] stopped_services_enable = ${stopped_services_enable}")
 
   $started_services = lookup('forthewin::services::started_services', Array[String], 'unique')
   $stopped_services = lookup('forthewin::services::stopped_services', Array[String], 'unique')
 
-  info("[${trusted[certname]}] started_services        = ${started_services}")
-  info("[${trusted[certname]}] stopped_services        = ${stopped_services}")
+  if $verbose {
+    info("[${trusted[certname]}] PARAMETERS:")
+    info("[${trusted[certname]}] started_services_enable = ${started_services_enable}")
+    info("[${trusted[certname]}] stopped_services_enable = ${stopped_services_enable}")
+    info("[${trusted[certname]}] VARIABLES:")
+    info("[${trusted[certname]}] started_services        = ${started_services}")
+    info("[${trusted[certname]}] stopped_services        = ${stopped_services}")
+  }
 
 	unless empty($started_services) {
     service { $started_services:
