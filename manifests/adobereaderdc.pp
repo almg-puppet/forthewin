718bdce480e7849b4f8c8b916c7335aa45ee8a91
Tiago M Delboni <tiago.delboni@almg.gov.br>
2023-09-12 16:07:00 -0300

    Updates Adobe ACROBAT if it is installed; if it is not, update Adobe READER

--- manifests/adobereaderdc.pp
+++ manifests/adobereaderdc.pp
@@ -1,28 +1,33 @@
 # Patches and installation sources: https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
 class forthewin::adobereaderdc (
+  Optional[String] $acrobat_update_filename = undef,
+  Pattern[/\A[0-9]+[.][0-9]+[.][0-9]+\Z/] $acrobat_update_version,
   Optional[String] $base_filename = undef,
   String $base_version = '15.007.20033',
   String $installer_path = "${forthewin::params::repo_basepath}\\adobe",
   String $lang = 'pt_BR',
-  Optional[String] $update_filename = undef,
-  Pattern[/\A[0-9]+[.][0-9]+[.][0-9]+\Z/] $update_version,
+  Optional[String] $reader_update_filename = undef,
+  Pattern[/\A[0-9]+[.][0-9]+[.][0-9]+\Z/] $reader_update_version,
   Boolean $verbose = $forthewin::params::verbose,
   ) inherits forthewin::params {
 
   if $verbose {
     info("[${trusted[certname]}] PARAMETERS:")
-    info("[${trusted[certname]}] base_filename   = ${base_filename}")
-    info("[${trusted[certname]}] base_version    = ${base_version}")
-    info("[${trusted[certname]}] installer_path  = ${installer_path}")
-    info("[${trusted[certname]}] lang            = ${lang}")
-    info("[${trusted[certname]}] update_filename = ${update_filename}")
-    info("[${trusted[certname]}] update_version  = ${update_version}")
+    info("[${trusted[certname]}] acrobat_update_filename = ${reader_update_filename}")
+    info("[${trusted[certname]}] acrobat_update_version  = ${reader_update_version}")
+    info("[${trusted[certname]}] base_filename           = ${base_filename}")
+    info("[${trusted[certname]}] base_version            = ${base_version}")
+    info("[${trusted[certname]}] installer_path          = ${installer_path}")
+    info("[${trusted[certname]}] lang                    = ${lang}")
+    info("[${trusted[certname]}] reader_update_filename  = ${reader_update_filename}")
+    info("[${trusted[certname]}] reader_update_version   = ${reader_update_version}")
   }
 
   # Version parsing
   $product = split($base_version, '[.]')[0] ? {'15' => 'DC', default => fail('Unsupported product! Check [base_version].')}
   $flat_base_version = join(split($base_version, '[.]'), '')
-  $flat_update_version = join(split($update_version, '[.]'), '')
+  $acrobat_flat_update_version = join(split($acrobat_update_version, '[.]'), '')
+  $reader_flat_update_version = join(split($reader_update_version, '[.]'), '')
 
 	# Set varibles based on existing facts
   $is_adobereaderdc_installed = $facts[is_adobereaderdc_installed]
@@ -37,65 +42,96 @@ class forthewin::adobereaderdc (
     $base_installer = "${installer_path}\\AcroRdrDC${flat_base_version}_${lang}.exe"
   }
 
-  # Set update installer path
-  if $update_filename {
-    $update_installer = "${installer_path}\\${update_filename}"
+  # Set update installer path for ACROBAT
+  if $acrobat_update_filename {
+    $acrobat_update_installer = "${installer_path}\\${acrobat_update_filename}"
   } else {
-    $update_installer = "${installer_path}\\AcroRdrDCUpd${flat_update_version}.msp"
+    $acrobat_update_installer = "${installer_path}\\AcrobatDCx64Upd${acrobat_flat_update_version}.msp"
+  }
+
+  # Set update installer path for READER
+  if $reader_update_filename {
+    $reader_update_installer = "${installer_path}\\${reader_update_filename}"
+  } else {
+    $reader_update_installer = "${installer_path}\\AcroRdrDCUpd${reader_flat_update_version}.msp"
   }
 
   if $verbose {
     info("[${trusted[certname]}] VARIABLES:")
-    info("[${trusted[certname]}] is_adobereaderdc_installed  = ${is_adobereaderdc_installed}")
-    info("[${trusted[certname]}] adobereaderdc_version       = ${adobereaderdc_version}")
-    info("[${trusted[certname]}] is_adobeacrobatdc_installed = ${is_adobeacrobatdc_installed}")
+    info("[${trusted[certname]}] acrobat_flat_update_version = ${acrobat_flat_update_version}")
+    info("[${trusted[certname]}] acrobat_update_installer    = ${acrobat_update_installer}")
     info("[${trusted[certname]}] adobeacrobatdc_version      = ${adobeacrobatdc_version}")
-    info("[${trusted[certname]}] flat_base_version           = ${flat_base_version}")
+    info("[${trusted[certname]}] adobereaderdc_version       = ${adobereaderdc_version}")
     info("[${trusted[certname]}] base_installer              = ${base_installer}")
-    info("[${trusted[certname]}] flat_update_version         = ${flat_update_version}")
-    info("[${trusted[certname]}] update_installer            = ${update_installer}")
+    info("[${trusted[certname]}] is_adobeacrobatdc_installed = ${is_adobeacrobatdc_installed}")
+    info("[${trusted[certname]}] is_adobereaderdc_installed  = ${is_adobereaderdc_installed}")
+    info("[${trusted[certname]}] flat_base_version           = ${flat_base_version}")
     info("[${trusted[certname]}] product                     = ${product}")
+    info("[${trusted[certname]}] reader_flat_update_version  = ${reader_flat_update_version}")
+    info("[${trusted[certname]}] reader_update_installer     = ${reader_update_installer}")
   }
 
-  unless $is_adobeacrobatdc_installed {
+  if $is_adobeacrobatdc_installed {
 
-    if $is_adobereaderdc_installed {
+    # https://www.puppet.com/docs/puppet/5.5/function#versioncmp
+    # versioncmp(a, b) returns
+    #   1 if version a is greater than version b
+    #   0 if the versions are equal
+    #   -1 if version a is less than version b
 
-      if versioncmp($update_version, $adobereaderdc_version) == 1 {
+    # Update ACROBAT only if newer than current version
+    if versioncmp($acrobat_update_version, $adobeacrobatdc_version) == 1 {
 
-        $exec_title = "Adobe Reader ${product} ${update_version}"
-        $exec_command = "C:\\Windows\\System32\\msiexec.exe /update ${update_installer} /qn"
-
-        if $verbose {
-          info("[${trusted[certname]}] exec_title   = ${exec_title}")
-          info("[${trusted[certname]}] exec_command = ${exec_command}")
-        }
-
-        exec { $exec_title:
-          command => $exec_command,
-        }
-
-      }
-
-    } else {
-
-      $language = $lang ? {'pt_BR' => ' - Português', default => ''}
-      $package_title = "Adobe Reader ${product} ${base_version}"
-      $package_name = "Adobe Reader ${product}${language}"
+      $acrobat_exec_title = "Adobe Acrobat ${acrobat_update_version}"
+      $acrobat_exec_command = "C:\\Windows\\System32\\msiexec.exe /update ${acrobat_update_installer} /qn"
 
       if $verbose {
-        info("[${trusted[certname]}] language      = ${language}")
-        info("[${trusted[certname]}] package_title = ${package_title}")
-        info("[${trusted[certname]}] package_name  = ${package_name}")
+        info("[${trusted[certname]}] acrobat_exec_title   = ${acrobat_exec_title}")
+        info("[${trusted[certname]}] acrobat_exec_command = ${acrobat_exec_command}")
       }
 
-      package { $package_title:
-        name            => $package_name,
-        ensure          => present,
-        source          => $base_installer,
-        install_options => ['/sAll']
+      exec { $acrobat_exec_title:
+        command => $acrobat_exec_command
       }
 
+    }
+
+  } elsif $is_adobereaderdc_installed {
+
+    # Update READER only if newer than current version
+    if versioncmp($reader_update_version, $adobereaderdc_version) == 1 {
+
+      $reader_exec_title = "Adobe Reader ${reader_update_version}"
+      $reader_exec_command = "C:\\Windows\\System32\\msiexec.exe /update ${reader_update_installer} /qn"
+
+      if $verbose {
+        info("[${trusted[certname]}] reader_exec_title   = ${reader_exec_title}")
+        info("[${trusted[certname]}] reader_exec_command = ${reader_exec_command}")
+      }
+
+      exec { $reader_exec_title:
+        command => $reader_exec_command
+      }
+    }
+
+  } else {
+    # Nothing installed - installl base version
+
+    $language = $lang ? {'pt_BR' => ' - Português', default => ''}
+    $package_title = "Adobe Reader ${product} ${base_version}"
+    $package_name = "Adobe Reader ${product}${language}"
+
+    if $verbose {
+      info("[${trusted[certname]}] language      = ${language}")
+      info("[${trusted[certname]}] package_title = ${package_title}")
+      info("[${trusted[certname]}] package_name  = ${package_name}")
+    }
+
+    package { $package_title:
+      name            => $package_name,
+      ensure          => present,
+      source          => $base_installer,
+      install_options => ['/sAll']
     }
 
   }
